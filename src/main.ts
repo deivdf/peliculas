68a394ace05094b197b358d13e76a04c2db40da5
D3ivdf <dfdavidfernando@gmail.com>
2024-12-22 15:30:56 -0500

    change: orm add and conection on bd

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import sequelize from './db/dbConect';
 
 async function bootstrap() {
+  sequelize
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
